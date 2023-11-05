20c12809c1e17c214e0aef301f8e924f23919840
Abhishek Patil <abishkpatil@gmail.com>
2023-11-05 20:09:31 +0530

    updated home page UI

--- frontend/src/navbar.jsx
+++ frontend/src/navbar.jsx
@@ -1,15 +1,16 @@
 import './App.css'
-import { Outlet, Link } from "react-router-dom";
+import {Link, useLocation } from "react-router-dom";
 
 
 function Navbar() {
+  const location = useLocation();
   return (
     <>
       <h1 className='py-5 text-center display-3 fw-medium' id="title"><u>QRcryption</u></h1>
       <div className="container">
         <div className='row text-center'>
-          <Link  to="/" className='col border border-3 rounded p-1 fs-4 px-3 py-2 me-5 border-danger navLinks'>Generate New QR Code</Link>
-          <Link  to="/checkstatus" className='col border border-3 rounded p-1 fs-4 px-3 py-2 ms-5 border-danger navLinks'>Check Status</Link>
+          <Link  to="/" className={`col border border-3 rounded p-1 fs-4 px-3 py-2 me-5 border-danger navLinks ${location.pathname === '/' ? 'navLinksActive' : 'navLinksInActive'}`}>Generate New QR Code</Link>
+          <Link  to="/checkstatus" className={`col border border-3 rounded p-1 fs-4 px-3 py-2 ms-5 border-danger navLinks ${location.pathname === '/checkstatus' ? 'navLinksActive' : 'navLinksInActive'}`}>Check Status</Link>
         </div>
       </div>
     </>
