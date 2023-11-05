ddbb5d548343b948c860fae6857f3e922a0f6a06
Abhishek Patil <abishkpatil@gmail.com>
2023-11-05 19:46:28 +0530

    added download button

--- frontend/src/checkStatus.jsx
+++ frontend/src/checkStatus.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 function CheckStatus() {
     const [orders, setOrders] = useState([]);
     const [error, setError] = useState(null);
+    const [imageData, setImageData] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:3000/api/all-orders')
@@ -14,6 +15,8 @@ function CheckStatus() {
             })
             .then((data) => {
                 console.log('Data received');
+                console.log(data);
+                // setImageData(data[0].imageData);
                 setOrders(data);
             })
             .catch((error) => {
@@ -21,46 +24,51 @@ function CheckStatus() {
                 setError(error);
             });
     }, []);
-    
+
 
     function getData() {
         return (
             <tbody>
-                {orders.map((val, index) => val.received === true ? (
+                {orders.map((val, index) => (
                     <tr key={index}>
                         <td scope="row">{val.orderID}</td>
                         <td>{val.receiverID}</td>
-                        <td className='bg-success-subtle'>{'Received'}</td>
-                        
-                    </tr>
-                ) : (
-                    <tr key={index}>
-                        <td scope="row">{val.orderID}</td>
-                        <td>{val.receiverID}</td>
-                        <td className='bg-danger-subtle'>{'Not Yet Received'}</td>
+                        <td className={val.received ? 'bg-success-subtle' : 'bg-danger-subtle'}>
+                            {val.received ? 'Received' : 'Not Yet Received'}
+                        </td>
                     </tr>
                 ))}
             </tbody>
         );
     }
+    
 
     return (
-        <div className="text-center" id="chkStatusDiv">
-            {error ? (
-                <div>Error: {error.message}</div>
-            ) : (
-                <table className="table table-striped container">
-                    <thead>
-                        <tr>
-                            <th scope="col">Order ID</th>
-                            <th scope="col">Receiver ID</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                    </thead>
-                    {getData()}
-                </table>
-            )}
+        <div>
+            <div className="text-center" id="chkStatusDiv">
+                {error ? (
+                    <div>Error: {error.message}</div>
+                ) : (
+                    <table className="table table-striped container">
+                        <thead>
+                            <tr>
+                                <th scope="col">Order ID</th>
+                                <th scope="col">Receiver ID</th>
+                                <th scope="col">Status</th>
+                                {/* <th scope="col">QR Code</th> */}
+                            </tr>
+                        </thead>
+                        {getData()}
+                    </table>
+                )}
+            </div>
+            {/* <div>
+                {imageData && (
+                    <img src={imageData} alt="Image" />
+                )}
+            </div> */}
         </div>
+
     );
 }
 
--- frontend/src/genNewCode.jsx
+++ frontend/src/genNewCode.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function GenerateNewQRCode() {
-const [orderID, setOrderID] = useState("");
-const [receiverID, setReceiverID] = useState("");
+  const [orderID, setOrderID] = useState("");
+  const [receiverID, setReceiverID] = useState("");
+  const [isImagePresent, setIsImagePresent] = useState(false);
 
-const handleGenerate = async () => {
+  const handleGenerate = async () => {
     // Send the orderID to your Node.js server for encryption
     const response = await fetch('http://localhost:3000/api/encrypt-link', {
       method: 'POST',
@@ -13,37 +14,54 @@ const handleGenerate = async () => {
       },
       body: JSON.stringify({ orderID, receiverID }),
     });
-  
+
     if (response.ok) {
       console.log('Data sent to backend');
+
+      const specificPath = "../public/";
+      const specificImageName = orderID + "_qr.png"
+      // Create an Image object and set its src
+      const image = new Image();
+      image.src = `${specificPath}${specificImageName}`;
+      console.log(image.scr);
+      // When the image loads, enable the download button
+      image.onload = () => {
+        setIsImagePresent(true);
+      };
     } else {
       console.error('Failed to send data to backend');
       // Handle errors if the request is not successful.
     }
   };
-  
+  const openImage = () => {
+    // Replace 'specificPath' and 'specificImageName' with your actual path and image name
+    const specificPath = "../public/";
+    const specificImageName = orderID + "_qr.png"
+    window.open(`${specificPath}${specificImageName}`, '_blank');
+  };
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
     console.log(orderID + ' ' + receiverID);
     handleGenerate();
-}
+  }
 
-    return <>
-        <form id="generateQRForm" className="border rounded-2 shadow-sm text-center" onSubmit={handleSubmit}>
-            <h2 class="mb-5">Enter Details</h2>
-            <div class="mb-4">
-                <label for="orderID" class="form-label fw-medium fs-5">Order ID</label>
-                <input type="number" class="form-control" id="orderID" value={orderID} onChange={(e) => setOrderID(e.target.value)}/>
-            </div>
-            <div class="mb-4">
-                <label for="receiverID" class="form-label fw-medium fs-5">Receiver ID</label>
-                <input type="number" class="form-control" id="receiverID" value={receiverID} onChange={(e) => setReceiverID(e.target.value)}/>
-            </div>
-            <button type="submit" class="btn fw-medium fs-5" id="generateBtn">Generate</button>
-        </form>
-    </>
+
+  return <>
+    <form id="generateQRForm" className="border rounded-2 shadow-sm text-center" onSubmit={handleSubmit}>
+      <h2 class="mb-5">Enter Details</h2>
+      <div class="mb-4">
+        <label for="orderID" class="form-label fw-medium fs-5">Order ID</label>
+        <input type="number" class="form-control" id="orderID" value={orderID} onChange={(e) => setOrderID(e.target.value)} />
+      </div>
+      <div class="mb-4">
+        <label for="receiverID" class="form-label fw-medium fs-5">Receiver ID</label>
+        <input type="number" class="form-control" id="receiverID" value={receiverID} onChange={(e) => setReceiverID(e.target.value)} />
+      </div>
+      <button type="submit" class="btn fw-medium fs-5" id="generateBtn">Generate</button>
+      <button type="button" class="btn fw-medium fs-5 mt-3" id="generateBtn" disabled={!isImagePresent} onClick={openImage}>Download</button>
+    </form>
+  </>
 }
 
 export default GenerateNewQRCode;
